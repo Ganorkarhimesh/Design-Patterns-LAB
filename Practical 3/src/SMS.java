638e8de0dc9c531c0c6c6cde93da4922f49cefc7
Himesh Ganorkar <ganorkarhimesh67@gmail.com>
2026-08-06 11:51:37 +0530

    Add files via upload

---
+++ Practical 3/src/SMS.java
@@ -0,0 +1,7 @@
+class SMS implements Notification {
+
+    public void sendNotification(String facultyName, String leaveType) {
+        System.out.println("SMS Sent to " + facultyName +
+                " : Your " + leaveType + " has been approved.");
+    }
+}
